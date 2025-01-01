a7fe6e01481f08b43f8a278dc6c18a9d864e5171
ADITYA RAJ <raj8017933@gmail.com>
2025-01-02 00:58:54 +0530

    prob 5

---
+++ C1-Microsoft/P5-Minimum_Moves_to_Equal_Array_Elements.cpp
@@ -0,0 +1,14 @@
+// Link: https://leetcode.com/problems/minimum-moves-to-equal-array-elements-ii/description/
+class Solution {
+public:
+    int minMoves2(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        int k=nums[n/2];
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans+=abs(nums[i]-k);
+        }
+        return ans;
+    }
+};
---
+++ C1-Microsoft/P6-Random_Point_in_Non-overlapping_Rectangles.cpp
@@ -0,0 +1,1 @@
+// Link: https://leetcode.com/problems/random-point-in-non-overlapping-rectangles/description/
