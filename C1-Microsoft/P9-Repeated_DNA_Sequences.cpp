f0d8b698594273b11fb5d9b3792421742e4e82d5
ADITYA RAJ <raj8017933@gmail.com>
2025-01-03 01:25:12 +0530

    prob 9

---
+++ C1-Microsoft/P9-Repeated_DNA_Sequences.cpp
@@ -0,0 +1,22 @@
+// Link: https://leetcode.com/problems/repeated-dna-sequences/description/
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        int n=s.size();
+        if(n<10)return {};
+        unordered_map<string,int>mpp;
+        int i=0;
+        while(i+9<n){
+            string temp=s.substr(i,10);
+            mpp[temp]++;
+            i++;
+        }
+        vector<string>ans;
+        for(auto it:mpp){
+            if(it.second>=2){
+                ans.push_back(it.first);
+            }
+        }
+        return ans;
+    }
+};
