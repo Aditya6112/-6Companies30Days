80ce105bbd46a6bb9051c7ffbdb2bfc77d099c0e
ADITYA RAJ <raj8017933@gmail.com>
2025-01-11 01:15:53 +0530

    prob 15

--- C2-Amazon/P15-Delete N nodes after M nodes of a linked list.cpp
+++ C2-Amazon/P15-Delete N nodes after M nodes of a linked list.cpp
@@ -1,2 +1,29 @@
 // Link: https://www.geeksforgeeks.org/problems/delete-n-nodes-after-m-nodes-of-a-linked-list/1
 
+class Solution {
+  public:
+    Node* linkdelete(Node* head, int n, int m) {
+        // code here
+        Node* node = head;
+        while(node!=nullptr)
+        {
+            Node* prev = nullptr;
+            // Skip m nodes
+            for(size_t id=0; id<m && node!=nullptr; id++)
+            {
+                prev = node;
+                node = node->next;
+            }
+            
+            // Delete n nodes
+            for(size_t id=0; id<n && node!=nullptr; id++)
+            {
+                Node* temp = node;
+                node = node->next;
+                delete temp;
+            }
+            prev->next = node;
+        }
+        return head;
+    }
+};
