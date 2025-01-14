bfc9c6b70e251c9175aa60682d642adfd2fb9535
ADITYA RAJ <raj8017933@gmail.com>
2025-01-15 00:35:38 +0530

    prob 9

---
+++ C3-Wallmart/P9-Verify Preorder Serialization of a Binary Tree.cpp
@@ -0,0 +1,16 @@
+// Link: https://leetcode.com/problems/verify-preorder-serialization-of-a-binary-tree/description/
+
+class Solution {
+public:
+    bool isValidSerialization(string preorder) {
+        stringstream ss(preorder);
+        string str;
+        int slots=1;
+        while(getline(ss,str,',')){
+            if(slots<=0)return false;
+            if(str=="#")slots-=1;
+            else slots+=1;
+        }
+        return slots==0;
+    }
+};
