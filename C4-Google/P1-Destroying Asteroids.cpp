dfc4d9779b422fed1c8a5b15efd52460e9283ac3
ADITYA RAJ <raj8017933@gmail.com>
2025-01-17 21:58:37 +0530

    prob 1

---
+++ C4-Google/P1-Destroying Asteroids.cpp
@@ -0,0 +1,16 @@
+// Link: https://leetcode.com/problems/destroying-asteroids/description/
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        sort(asteroids.begin(), asteroids.end());
+        long long currentMass = mass; 
+        for (int asteroid : asteroids) {
+            if (currentMass < asteroid) {
+                return false;
+            }
+            currentMass += asteroid;
+        }
+        
+        return true;
+    }
+};
