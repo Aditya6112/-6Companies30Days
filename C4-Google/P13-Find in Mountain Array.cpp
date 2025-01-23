ae1d37f09c4ef8bac6b548d21e1fb993ca3cde5c
ADITYA RAJ <raj8017933@gmail.com>
2025-01-23 19:24:05 +0530

    prob 13

---
+++ C4-Google/P13-Find in Mountain Array.cpp
@@ -0,0 +1,45 @@
+// Link: https://leetcode.com/problems/find-in-mountain-array/description/
+class Solution {
+public:
+    int findInMountainArray(int target, MountainArray &mountainArr) {
+        int n = mountainArr.length();
+        // Find the peak index
+        int left = 0, right = n - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (mountainArr.get(mid) < mountainArr.get(mid + 1)) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        int peak = left;
+        //left
+        int start = 0;
+        int end = peak;
+        while (start <= end) { // Corrected condition
+            int mid = start + (end - start) / 2;
+            if (mountainArr.get(mid) == target) {
+                return mid;
+            } else if (mountainArr.get(mid) > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        //right
+        start = peak;
+        end = mountainArr.length() - 1;
+        while (start <= end) { // Corrected condition
+            int mid = start + (end - start) / 2;
+            if (mountainArr.get(mid) == target) {
+                return mid;
+            } else if (mountainArr.get(mid) > target) {
+                start = mid + 1; // Corrected direction
+            } else {
+                end = mid - 1; // Corrected direction
+            }
+        }
+        return -1;
+    }
+};
